c70037fd060528f6764df8aac98f5831e2f9344a
Franz Kramer da Silva <franzkramerfr@gmail.com>
2026-05-15 08:04:28 -0300

    Inicio Projeto Elevador

---
+++ Franz - Python/Aula 11/decimo_primeiro.py
@@ -0,0 +1,14 @@
+#Explicação de def: A palavra-chave "def" é usada em Python para definir uma função. Uma função é um bloco de código reutilizável que realiza uma tarefa específica.
+#return: A palavra-chave "return" é usada para finalizar a execução de uma função e retornar um valor para o local onde a função foi chamada. O valor retornado pode ser usado poseriormente no código.
+
+#Exemplo de função com def e return:
+
+def nome_da_funcao(parametro1, parametro2):
+    # Corpo da função (código que será executado)
+    resultado = parametro1 + parametro2
+    return resultado
+
+def saudacao(nome):
+    return f"Olá, {nome}!"
+
+def nome():
---
+++ Franz - Python/Projetos/projeto_elevador.py
@@ -0,0 +1,19 @@
+#Sistema de elevador de prédio
+# o prédio possui 10 andares, sendo o térreo o andar 0. O elevador pode
+# se mover para cima ou para baixo, e tem a capacidade de tranasportar até 5 pessoas
+# O elevador começa no andar 0 e pode ser chamado por qualquer pessoa em qualquer andar.
+# O elevador deve se mover para o andar onde a pessoa chamou, e depois para o andar destino da pessoa
+# O elevador deve exibir mensagens indicando o andar atula, o número de pessoas no elevador, e as ações realizadas
+# O programa deve continuar rodando até que o usuario dedicada encerrar.
+
+# Requisitos Funcionais (RF):
+# O elevador deve se mover para cima e para baixo
+# O elevador deve se mover para o andar onde a pessoa chamou, e depois para o andar destino da pessoa.
+# O elevador possui uma capacidade limite de pessoas
+# O elevador deve exibir mensagens indicando o andar atula, o número de pessoas no elevador, e as ações realizadas
+
+# Requisitos Não Funcionais (RNF):
+# O elevador deve exibir mensagens indicando o andar atula, o número de pessoas no elevador, e as ações realizadas.
+# O programa pode continuar rodando até que o usuario dedicada encerrar.
+# O elevador pode começar do andar 0.
+
